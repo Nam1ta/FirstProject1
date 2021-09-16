7319ccb23eed33682cf97144f250d475ffcd7527
Nam1ta <90744752+Nam1ta@users.noreply.github.com>
2021-09-16 16:33:11 +0530

    Update newp.py

--- newp.py
+++ newp.py
@@ -1,3 +1,3 @@
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path="c:\selenium browser drivers\chromedriver.exe")
-driver.get("https://www.google.com/")
+driver.get("https://www.google.com/")namita
